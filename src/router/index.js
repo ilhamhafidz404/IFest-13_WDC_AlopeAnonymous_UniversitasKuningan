df75f59b3a5d1fb1c90f22cd4407d2a5b02e0ae9
dikrifzn <dikrifauz@gmai.com>
2025-03-01 02:01:58 +0700

    Page Login Register

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Home from "../views/home.vue";
+import Login from "../views/login.vue";
+import Register from "../views/register.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -8,6 +10,16 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: Home,
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login,
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: Register,
     },
   ],
 });
